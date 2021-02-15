13baf5a54bda12a67f3e02f6d175547af9d5f23e
saikiran-dev <mskiran1995@gmail.com>
2021-02-15 23:22:54 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const previous = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+});
+
+previous.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive > 1) {
+    previous.disabled = false;
+  } else {
+    previous.disabled = true;
+  }
+
+  if (actives.length === circles.length) {
+    next.disabled = true;
+  } else {
+    next.disabled = false;
+  }
+}
